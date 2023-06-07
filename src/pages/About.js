dc72622d7eec3cb0c3e586dd63e8a1899700f74f
tatianajoli <tatiana.joli@outlook.com>
2023-06-07 19:48:36 -0400

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import pfp from "src/assets/pfp.jpg";
+
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p>
+          
+        <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Your Name</div>
+        <div className="brief_description">
+          // Details about you goes here
+        </div>
+      </div>
+    </
+
+        </p> 
       </div>
     )
   }
