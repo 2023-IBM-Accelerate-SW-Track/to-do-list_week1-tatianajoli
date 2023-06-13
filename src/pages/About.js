2c27f18986dbc8ec8f4a64016e54357797b75ebf
tatianajoli <tatiana.joli@outlook.com>
2023-06-12 21:54:06 -0400

    passed tests, added personalizations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import pfp from "src/assets/pfp.jpg";
+import pfp from "../assets/pfp.jpg";
 
   
 export default class About extends Component {
@@ -9,23 +9,24 @@ export default class About extends Component {
         <p>
           
         <div>
-    <div className="split left">
-      <div className="centered">
-        <img 
-          className="profile_image"
-          // Image goes here
-          alt="Profile Pic"
-          ></img>
-      </div>
-    </div>
-    <div className="split right">
-      <div className="centered">
-        <div className="name_title">Your Name</div>
-        <div className="brief_description">
-          // Details about you goes here
+          <div className="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src={pfp}
+                alt="Profile Pic"
+                ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Your Name</div>
+              <div className="brief_description">
+                // Details about you goes here
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </
 
         </p> 
       </div>
